5799aacd8143c597a237e56894f0a9b34cbb2976
Sirui Lu <lsrcz@pku.edu.cn>
2019-05-31 11:14:10 +0800

    fix
    Signed-off-by: Sirui Lu <lsrcz@pku.edu.cn>

--- src/main/java/layers/regularizer/BaseRegularizer.java
+++ src/main/java/layers/regularizer/BaseRegularizer.java
@@ -1,4 +1,34 @@
 package layers.regularizer;
 
-public class BaseRegularizer {
+import layers.ConfigurableObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BaseRegularizer extends ConfigurableObject {
+
+    public final static Map<String, Class<?>> map;
+
+    static {
+        map = new HashMap<>();
+    }
+
+    public static String[] getSelections() {
+        String[] ret = new String[map.size()];
+        map.keySet().toArray(ret);
+        return ret;
+    }
+
+    public static BaseRegularizer select(String str) {
+        if (map.containsKey(str)) {
+            Class<?> cls = map.get(str);
+            try {
+                return (BaseRegularizer) cls.newInstance();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                System.exit(-1);
+            }
+        }
+        return null;
+    }
 }
